632190863d3f2fd9c19add7fef7be34f4b8d9130
MrLYC <imyikong@gmail.com>
2015-08-14 18:47:28 +0800

    add favicon.ico

--- banbrick/banbrick/urls.py
+++ banbrick/banbrick/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'banbrick.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url("favicon.ico", RedirectView.as_view(url="static/favicon.ico")),
 
     url(r'^panel/', include('xpanel.urls')),
 )
