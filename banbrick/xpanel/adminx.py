2b09cc4f297f547dfd2a05e230b478be6382f71d
MrLYC <imyikong@gmail.com>
2015-08-14 18:20:58 +0800

    update panel ui

--- banbrick/xpanel/adminx.py
+++ banbrick/xpanel/adminx.py
@@ -1,6 +1,23 @@
 from core import models
 
 import xadmin
+from xadmin import views
+
+
+class BaseAdminViewSetting(object):
+    enable_themes = True
+    use_bootswatch = True
+
+xadmin.site.register(views.BaseAdminView, BaseAdminViewSetting)
+
+
+class CommAdminViewSetting(object):
+    global_models_icon = {
+        models.ProjectTag: 'fa fa-tag', models.MonitorItemTag: 'fa fa-tag',
+        models.Project: 'fa fa-laptop', models.MonitorItem: 'fa fa-magic',
+    }
+
+xadmin.site.register(views.CommAdminView, CommAdminViewSetting)
 
 
 class SimpleModelAdmin(object):
