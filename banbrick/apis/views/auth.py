84061e94b0b97c1eb3dcbe2dda5e1798958e4c72
MrLYC <imyikong@gmail.com>
2015-08-16 17:38:39 +0800

    refresh auth token every time

--- banbrick/apis/views/auth.py
+++ banbrick/apis/views/auth.py
@@ -17,47 +17,16 @@ class ApiAuthView(ObtainAuthToken):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        now = datetime_now()
-        tokens = list(Token.objects.filter(
+        Token.objects.filter(
             user=serializer.validated_data['user'],
-        ).order_by('created'))
-        for token in tokens:
-            if is_token_expired(token, now):
-                token.delete()
-            else:
-                break
-        else:
-            token = Token.objects.create(
-                created=now, user=serializer.validated_data['user'],
-            )
+        ).delete()
+
+        now = datetime_now()
+        token = Token.objects.create(
+            created=now, user=serializer.validated_data['user'],
+        )
 
         return Response({'token': token.key})
-
-    def put(self, request):
-        auth_key = request.data.get("auth", "")
-        authenticate = TokenAuthentication()
-        try:
-            user, token = authenticate.authenticate_credentials(auth_key)
-        except exceptions.AuthenticationFailed as err:
-            return Response({
-                "ok": False,
-                "detail": err.detail,
-            }, status=status.HTTP_403_FORBIDDEN)
-
-        now = datetime_now()
-        if is_token_expired(token, now):
-            token.delete()
-            return Response({
-                "ok": False,
-                "detail": "token was expired",
-            }, status=status.HTTP_403_FORBIDDEN)
-
-        token.created = now
-        token.save()
-        return Response({
-            "ok": True,
-            "detail": token.created,
-        }, status=status.HTTP_202_ACCEPTED)
 
     def delete(self, request):
         auth_key = request.data.get("auth", "")
