351ef3c77e2d4c309b919f2a52bc8090e37bf370
MrLYC <imyikong@gmail.com>
2015-08-16 18:48:58 +0800

    hook SIGINT and send it to ipython

--- banbrick/banbrick/management/commands/ipy.py
+++ banbrick/banbrick/management/commands/ipy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import os
+import signal
+import sys
+
 from ycyc.base.shelltools import ShellCommands
 from django.core.management.base import BaseCommand, CommandError
 
@@ -9,4 +13,11 @@ class Command(BaseCommand):
     help = 'update the database models'
 
     def handle(self, *args, **options):
-        return ShellCommands.ipython.check_call()
+        ipython = ShellCommands.ipython()
+
+        def signal_to_ipy(sig, s):
+            os.kill(ipython.pid, sig)
+
+        signal.signal(signal.SIGINT, signal_to_ipy)
+        ipython.communicate()
+        sys.exit(ipython.poll())
