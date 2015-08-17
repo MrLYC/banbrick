bdbc9f8ecadaa44b7c5867ca11e3144a90f92277
MrLYC <imyikong@gmail.com>
2015-08-18 00:10:40 +0800

    add CsrfViewMiddleware

--- banbrick/banbrick/settings.py
+++ banbrick/banbrick/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
