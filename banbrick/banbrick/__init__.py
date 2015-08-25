18c1f61a173993cf49f18b794d0864dce6fef129
MrLYC <imyikong@gmail.com>
2015-08-25 17:25:36 +0800

    add a setting to use gevent money patch

--- banbrick/banbrick/__init__.py
+++ banbrick/banbrick/__init__.py
@@ -1,5 +1,9 @@
 import django
 
+if getattr(django.conf.settings, "GEVENTPATCH", False):
+    from gevent import monkey
+    monkey.patch_all()
+
 from ycyc.shortcuts import logger_quick_config
 
 django.setup()
