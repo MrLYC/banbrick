6ecaac1769e4074f7f64429d1b6aae0c7d9a8714
MrLYC <imyikong@gmail.com>
2015-08-28 17:57:47 +0800

    rename

--- banbrick/banbrick/settings/__init__.py
+++ banbrick/banbrick/settings/__init__.py
@@ -1,7 +1,7 @@
 from ycyc.base.funcutils import export_module
 from ycyc.base.txtutils import sep_join
 
-for setting_name in ("prod", "dev", "demo"):
+for setting_name in ("prod", "dev", "base"):
     try:
         export_module(sep_join(".", [__name__, setting_name]))
         break
