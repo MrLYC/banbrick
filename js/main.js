25308b18fcde305dbed8d7034f1e09a2da0de00d
MrLYC <imyikong@gmail.com>
2015-09-25 05:40:45 +0000

    update

--- js/main.js
+++ js/main.js
@@ -2,22 +2,43 @@
     var gh_api = null;
     var toc_tpl = null;
     var md_converter = null;
-    var doc_list = null;
-    var dir_list = null;
-    var active_toc_item= null;
+    var toc_map = null;
     
     var $toc_list = null;
     var $article_area = null;
     var $title = null;
 
     function render_toc(toc){
+        toc_map = {};
+        
+        for(var i in toc){
+            var item = toc[i];
+            toc_map[item.sha] = item;
+        }
+        
         $toc_list.html(toc_tpl({
             toclist: toc
         }));
+        
+        $(".toc-lnk").click(function(argument) {
+            var self = $(this);
+            var sha = self.data("sha");
+            var item = toc_map[sha];
+            active_item(item);
+        })
     }
     
     function render_markdown(text) {
         $article_area.html(md_converter.makeHtml(text));
+        var $pre_area = $article_area.find("pre");
+        $pre_area.addClass("hljs");
+        $pre_area.find("code").each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+        $article_area.find("h1, h2, h3, h4, h5").each(function() {
+            var self = $(this);
+            self.attr("id", escape(self.text()));
+        });
     }
     
     function list_dir(dir_name, callback) {
@@ -30,15 +51,20 @@
                 var toc_list = [];
                 for(var i in data){
                     var item = data[i];
-                    toc_list.push({
-                        name: item["name"],
-                        url: item["download_url"],
-                        type: item["type"],
-                        size: item["size"],
-                        path: item["path"],
-                        sha: item["sha"],
-                    });
+                    if(item.type == "file" && item.name.endsWith(".md")){
+                        toc_list.push({
+                            name: item["name"],
+                            url: item["download_url"],
+                            type: item["type"],
+                            size: item["size"],
+                            path: item["path"],
+                            sha: item["sha"],
+                        });
+                    }
                 }
+                toc_list.sort(function(a, b) {
+                    return a > b;
+                })
                 callback(toc_list);
             }
         );
@@ -56,8 +82,10 @@
     }
     
     function active_item(item) {
+        fetch_doc(item);
         $(".active-toc-item").removeClass("active-toc-item");
         $("#toc_item_" + item.sha).addClass("active-toc-item");
+        $("#content").scrollTop(0);
     }
     
     $(function init(){
@@ -68,23 +96,32 @@
         $toc_list = $("#toc-list");
         $article_area = $("#article");
         $title = $("title");
+        
+        hljs.initHighlightingOnLoad();
     
         render_markdown($("#md-default").text());
         list_dir(META.directory, function(data) {
-            dir_list = data;
-            doc_list = [];
+            var selected = document.location.hash.substr(1);
+            var active_toc_item = null;
             for(var i in data){
                 var item = data[i];
                 if(item.name == META.default){
-                    fetch_doc(item);
+                    data.splice(i, 1);
+                    data.splice(0, 0, item);
+                    if(selected.length == 0){
+                        active_toc_item = item;
+                        break;
+                    }
+                }
+                if(selected.length > 1 && item.name == selected){
                     active_toc_item = item;
                 }
-                if(item.type == "file" && item.name.endsWith(".md")){
-                    doc_list.push(item);
-                }
             }
-            render_toc(doc_list);
-            active_item(active_toc_item);
+            
+            render_toc(data);
+            if(active_toc_item != null){
+                $(".toc-lnk[data-sha=" + active_toc_item.sha + "]").click();
+            }
         });
     });
 })();
